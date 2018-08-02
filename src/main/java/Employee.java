afd1fb906a04064672646aefcada5023c86eb061
Joanna Gora <joanna.gora@protonmail.com>
2018-08-02 15:35:20 +0100

    initial commit, created file structure and constructor for employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+    private String name;
+    private int niNumber;
+    private int salary;
+
+    public Employee(String name, int niNumber, int salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+
+    }
+}
